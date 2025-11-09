ee8e03db28e651b94ae9c62b053dd57041129010
gitFa07 <office.falakanwar@gmail.com>
2025-11-10 00:29:55 +0530

    Updated user authentication: added bcrypt hashing, JWT login, and fixed model schema

--- routes/user/controller.js
+++ routes/user/controller.js
@@ -1,10 +1,57 @@
+// routes/user/controller.js
 const model = require('./model');
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
 
 module.exports = {
     login: (req, res) => {
-        res.status(200).send({msg: 'Login Successful'})
+        model.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    return res.status(401).send({ msg: 'Invalid email or password' });
+                }
+
+                user.comparePassword(req.body.password, (err, isMatch) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send({ msg: 'Internal server error' });
+                    }
+
+                    if (isMatch) {
+                        let token = jwt.sign({ _id: user._id }, config.secret, { expiresIn: 86400 });
+                        return res.status(200).send({ msg: 'Login Successful', token });
+                    } else {
+                        return res.status(401).send({ msg: 'Passwords did not match' });
+                    }
+                });
+            })
+            .catch(err => {
+                console.error('DB error:', err);
+                res.status(500).send({ msg: 'Internal server error' });
+            });
     },
+
     register: (req, res) => {
-        res.status(200).send({msg: 'Register Successful'})
+        let newUser = new model({
+            forename: req.body.forename,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        newUser.save()
+            .then(result => {
+                console.log(result);
+                res.status(201).send({ msg: 'Register Successful', user_id: result._id });
+            })
+            .catch(err => {
+                console.error(err);
+                // handle duplicate email more gracefully
+                if (err.code === 11000) {
+                    return res.status(409).send({ msg: 'Email already registered' });
+                }
+                res.status(500).send({ msg: 'Register Unsuccessful' });
+            });
+
     }
-}
+};
--- routes/user/model.js
+++ routes/user/model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     forename: {
@@ -15,10 +16,39 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     account_created: {
         type: String,
         default: Date.now()
     }
-})
+});
+
+userSchema.pre('save', function (next) {
+    let user= this;
+
+    if(!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password= hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+const userModel= mongoose.model('user', userSchema);
+
+module.exports= userModel;
